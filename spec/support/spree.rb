e6ddc4fbf247afe134b2138b2ac310d529a4a4b3
tyler <84726170+embold-tyler@users.noreply.github.com>
2025-07-29 09:55:54 -0700

    Fix date formatting in OverviewController and specs, fix factories require statement

--- spec/support/spree.rb
+++ spec/support/spree.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'spree/testing_support/capybara_ext'
-require 'spree/testing_support/factories'
+require 'spree/testing_support/factory_bot'
 require 'spree/testing_support/controller_requests'
 require 'spree/testing_support/authorization_helpers'
 require 'spree/testing_support/url_helpers'
